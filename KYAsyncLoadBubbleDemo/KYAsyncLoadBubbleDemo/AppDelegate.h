cdad785d32d9d99cadf92a8592025d784dd73902
KittenYang <kittenyang@icloud.com>
2015-07-01 22:35:24 +0800

    first commit

---
+++ KYAsyncLoadBubbleDemo/KYAsyncLoadBubbleDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KYAsyncLoadBubbleDemo
+//
+//  Created by Kitten Yang on 7/1/15.
+//  Copyright (c) 2015 Kitten Yang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
